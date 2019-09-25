e23d17c4923b4cf7982cafe4d9147b5c7566e9d0
seenu <vasu461@gmail.com>
2019-09-24 21:17:56 -0500

    updated

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import "./styles.css";
+
+class App extends Component {
+    render() {
+        return (
+            <div>
+                <div className="App-header">
+                    <h2>Welcome to React</h2>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default App;
